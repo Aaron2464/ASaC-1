768faac794cfc3989df705d14b52be7e1a442106
Aaron <tim2464@yahoo.com.tw>
2018-02-10 21:37:08 +0800

    [ADD]Create jobs to choose

--- AssignmentsW1/app/src/main/java/com/example/aaron/assignmentsw1/Human.java
+++ AssignmentsW1/app/src/main/java/com/example/aaron/assignmentsw1/Human.java
@@ -1,13 +1,42 @@
 package com.example.aaron.assignmentsw1;
 
+import java.util.Scanner;
+
 public class Human {
 
+   Scanner scanner = null;
    private String name;
    public final static int FIRE_BOW = 0x01;
    public final static int ICE_BOW = 0x02;
 
    public Human(){
 
+   }
+
+   public void Jobs(){
+      System.out.println("There is some jobs that you can choose.");
+      System.out.println("(1)Mage(2)Hunter(3)Warrior");
+      System.out.print("Type the number to choose your jobs:  ");
+      int Job = scanner.nextInt();
+      switch (Job) {
+         case 1:
+         {
+            break;
+         }
+         case 2:
+         {
+            break;
+         }
+         case 3:
+         {
+            break;
+         }
+         default:
+         {
+            System.out.printf("");
+            break;
+         }
+      };
    }
 
    public void attack(){
