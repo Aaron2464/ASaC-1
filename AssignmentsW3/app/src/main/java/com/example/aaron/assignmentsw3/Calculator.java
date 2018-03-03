3b9232d37e1c0230f4da5f8d5f5f23db0bc2d830
Aaron <tim2464@yahoo.com.tw>
2018-03-03 10:00:52 +0800

    [ADD] The function of Equals

--- AssignmentsW3/app/src/main/java/com/example/aaron/assignmentsw3/Calculator.java
+++ AssignmentsW3/app/src/main/java/com/example/aaron/assignmentsw3/Calculator.java
@@ -9,12 +9,16 @@ import android.widget.TextView;
 public class Calculator extends AppCompatActivity {
 
     private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnC, btnMu, btnS, btnPC, btnCS, btnADD, btnPo ,btnD, btnE;
-    private TextView answer, txtshow;
+    private TextView answerView, txtshow;
+    private double sum = 0;
+    private double count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        answerView = (TextView)findViewById(R.id.answerView);
         txtshow = (TextView)findViewById(R.id.txtShow);
         btn0 = (Button)findViewById(R.id.btn0); btn0.setOnClickListener(MybtnListener);
         btn1 = (Button)findViewById(R.id.btn1); btn1.setOnClickListener(MybtnListener);
@@ -42,46 +46,48 @@ public class Calculator extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             String show = txtshow.getText().toString();
+            String[] numArray = show.split(" ");  //設定陣列長度為字串長度 利用空白切割字串
             Button btn = (Button)findViewById(v.getId());
             switch (v.getId()){
                 case R.id.btnC:
                     txtshow.setText("");
+                    answerView.setText("");
                     break;
                 case R.id.btnADD:
                     String lastStr = show.substring(show.length()-1,show.length());
-                    if (lastStr.equals("+") || lastStr.equals("-") || lastStr.equals("×") || lastStr.equals("÷")) {
+                    if (lastStr.equals(" ")) {
 
                     } else {
-                        txtshow.setText(show + "+");
+                        txtshow.setText(show + " + ");
                     }
                     break;
                 case R.id.btnS:
                     lastStr = show.substring(show.length()-1,show.length());
-                    if (lastStr.equals("+") || lastStr.equals("-") || lastStr.equals("×") || lastStr.equals("÷")) {
+                    if (lastStr.equals(" ")) {
 
                     } else {
-                        txtshow.setText(show + "-");
+                        txtshow.setText(show + " - ");
                     }
                     break;
                 case R.id.btnMu:
                     lastStr = show.substring(show.length()-1,show.length());
-                    if (lastStr.equals("+") || lastStr.equals("-") || lastStr.equals("×") || lastStr.equals("÷")) {
+                    if (lastStr.equals(" ")) {
 
                     } else {
-                        txtshow.setText(show + "×");
+                        txtshow.setText(show + " × ");
                     }
                     break;
                 case R.id.btnD:
                     lastStr = show.substring(show.length()-1,show.length());
-                    if (lastStr.equals("+") || lastStr.equals("-") || lastStr.equals("×") || lastStr.equals("÷")) {
+                    if (lastStr.equals(" ")) {
 
                     } else {
-                        txtshow.setText(show + "÷");
+                        txtshow.setText(show + " ÷ ");
                     }
                     break;
                 case R.id.btnPo:
                     lastStr = show.substring(show.length()-1,show.length());
-                    if (lastStr.equals("+") || lastStr.equals("-") || lastStr.equals("×") || lastStr.equals("÷")) {
+                    if (lastStr.equals(" ")) {
                         txtshow.setText(show + "0.");
                     }else if(lastStr.equals(".")){
 
@@ -91,13 +97,73 @@ public class Calculator extends AppCompatActivity {
                     }
                     break;
                 case R.id.btn0:
-                    if (show.equals("0")||show.equals("+") || show.equals("-") || show.equals("×") || show.equals("÷")) {
+                    if (show.equals("0")||show.equals(" ")) {
                         txtshow.setText("0");
                     } else {
                         txtshow.setText(show + "0");
                     }
                     break;
                 case R.id.btnE:
+                    sum = 0;
+                    count = 0;
+                    lastStr = show.substring(show.length()-1,show.length());
+                    if (lastStr.equals(" ")) {
+                        answerView.setText("ERROR");
+                    }else{
+                        sum = Double.parseDouble(numArray[0]);
+                        for (int i = 1 ; i < numArray.length ; i = i + 2){
+                            if(numArray[i].equals("×")){
+                                sum = sum * Double.parseDouble(numArray[i+1]);
+                            }else if(numArray[i].equals("÷")){
+                                sum = sum / Double.parseDouble(numArray[i+1]);
+                            }else if(numArray[i].equals("+")) {
+                                if ((i+2) == numArray.length){
+                                    sum = sum + Double.parseDouble(numArray[i + 1]);
+                                }else{
+                                    if (numArray[i + 2].equals("×") || numArray[i + 2].equals("÷")) {
+                                        count = Double.parseDouble(numArray[i + 1]);
+                                        for(int j = i + 2; j < numArray.length ; j =  j + 2) {
+                                            if (numArray[j].equals("×")) {
+                                                count = count * Double.parseDouble(numArray[j + 1]);
+                                                i = i + 2;
+                                            } else if(numArray[j].equals("÷")){
+                                                count = count / Double.parseDouble(numArray[j + 1]);
+                                                i = i + 2;
+                                            }else {
+                                                break;
+                                            }
+                                        }
+                                        sum = sum + count;
+                                    }else{
+                                        sum = sum + Double.parseDouble(numArray[i + 1]);
+                                    }
+                                }
+                            }else if(numArray[i].equals("-")) {
+                                if ((i + 2) == numArray.length) {
+                                    sum = sum - Double.parseDouble(numArray[i + 1]);
+                                } else {
+                                    if (numArray[i + 2].equals("×") || numArray[i + 2].equals("÷")) {
+                                        count = Double.parseDouble(numArray[i + 1]);
+                                        for (int j = i + 2; j <= numArray.length; j = j + 2) {
+                                            if (numArray[j].equals("×")) {
+                                                count = count * Double.parseDouble(numArray[j + 1]);
+                                                i = i + 2;
+                                            } else if (numArray[j].equals("÷")) {
+                                                count = count / Double.parseDouble(numArray[j + 1]);
+                                                i = i + 2;
+                                            } else {
+                                                break;
+                                            }
+                                        }
+                                        sum = sum - count;
+                                    } else {
+                                        sum = sum - Double.parseDouble(numArray[i + 1]);
+                                    }
+                                }
+                            }
+                        }
+                        answerView.setText(Double.toString(sum));
+                    }
                     break;
                 default:
                     txtshow.setText(show + btn.getText());
@@ -105,5 +171,7 @@ public class Calculator extends AppCompatActivity {
             }
         }
     };
-
 }
+
+
+//增加 % & +/- ，排除第二個小數點的問題
